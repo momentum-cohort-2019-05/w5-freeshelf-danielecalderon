63c07b3b44d703426a3244a776fb6785e7ed9f9e
Daniel E Calderon <Daniel.Calderon06@gmail.com>
2019-06-26 16:16:25 -0400

    CVS file used

---
+++ Freeshelf/book/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    url = models.URLField(max_length=250)
+    description = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
