5940b6706020af2d161337eec63aa7a1bfb25653
Daniel E Calderon <Daniel.Calderon06@gmail.com>
2019-06-27 15:34:14 -0400

    added models

--- Freeshelf/book/views.py
+++ Freeshelf/book/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render
 from .models import Book
+
+
 def index(request):
+    book_list = Book.objects.all()
     
-    books = Book.objects.all()
-    
-    response = render(request, 'index.html', {
-        'books': books,
-         
-    })
+    context = { 'book_list': book_list
+    }
+    return render(request, 'index.html' , context=context)
 
-    return response
-
-def book_detail(request,):
-    book = Book.objects.get()
-    return render(request, 'books/book_detail.html', {
-        'book': book,
-    })
+# def book_detail(request,):
+#     book = Book.objects.get()
+#     return render(request, 'books/book_detail.html', {
+#         'book': book,
+#     })
